eec83c00c3b4ae390329af73dd10e28484b9e163
Quan <quan@okvip.com>
2023-06-08 17:17:27 +0700

    bai 19

--- src/App.js
+++ src/App.js
@@ -5,27 +5,54 @@ import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
 
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+class App extends React.Component {
 
-  return (
-    <div>
-      Hello Quan
-      <MyComponent></MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Quan nè
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+  state = {
+    name: 'Khang',
+    address: 'Dan Cong Nghe',
+    age: 32
+  };
+
+
+  handleClick(event) {
+    console.log(">>> Click me my button")
+    console.log("My name is", this.state.name)
+  }
+
+  handleOnMoverOver(event) {
+    console.log(event.pageX)
+  }
+  //JSX
+  render() {
+    return (
+      <div>
+        My name is {this.state.name} and i'm from {this.state.address}
+        <button onMouseOver={this.handleOnMoverOver}>Hover me</button>
+        <button onClick={this.handleClick}>Click me</button>
+      </div>
+    );
+  }
 }
+
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Quan nè
+//     //     </p>
+//     //     <div>Count = {count}</div>
+//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//     //   </header>
+//     // </div>
+//   );
+// }
 
 export default App;
