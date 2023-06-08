4db9abf2606bbd2f0e99f350c425100ee04c65dd
Quan <quan@okvip.com>
2023-06-08 12:55:12 +0700

    bai 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,17 @@
+// class component
+// function component
+
+import React from "react";
+
+class MyComponent extends React.Component {
+    //JSX
+    render() {
+        return (
+            <div>My First Component
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
