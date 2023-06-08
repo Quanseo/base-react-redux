bf843338ad26e3414e9d64c0b9841673ad8c8c22
Quan <quan@okvip.com>
2023-06-08 18:07:08 +0700

    bai 21

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,14 +4,44 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'Khang',
+        address: 'Dan Cong Nghe',
+        age: 32
+    };
+
+
+    handleClick(event) {
+        console.log(">>> Click me my button");
+
+        //merge State => react class
+        this.setState({
+            name: 'Quan',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+        // this.setState({
+        //   age: Math.floor((Math.random() * 100) + 1)
+        // })
+    }
+
+    handleOnMoverOver(event) {
+        console.log(event.pageX)
+    }
     //JSX
     render() {
         return (
-            <div>My First Component
-                {Math.random()}
+            <div>
+                My name is {this.state.name} and i'm {this.state.age}
+                <button onClick={(event) => { this.handleClick(event) }}>Click me</button>
+                <form>
+                    <input type="text" />
+                    <button>Submit</button>
+                </form>
             </div>
         );
     }
 }
+
+
 
 export default MyComponent;
