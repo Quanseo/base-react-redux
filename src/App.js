4776c03499029ce668c298d603d6dc5ab367e496
Quan <quan@okvip.com>
2023-06-12 17:46:54 +0700

    bai 48

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import { Link } from "react-router-dom";
 
 const App = () => {
 
@@ -8,8 +9,19 @@ const App = () => {
 
     <div className="app-container">
       <Header />
+      <div>
+        test Link
+        <div>
+          <button>
+            <Link to="/users">Go To User Page</Link>
+          </button>
+          <button>
+            <Link to="/admins">Go To Admin Page</Link>
+          </button>
+        </div>
+      </div>
 
-    </div>
+    </div >
   );
 }
 
