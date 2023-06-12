0932e57b7783dd811a2fdab88c5b1fe894a465e0
Quan <quan@okvip.com>
2023-06-12 11:56:50 +0700

    Bai 46

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
 
-class App extends React.Component {
-
-  render() {
-    return (
-      <div className='app-container'>
-        Quaan
-        <MyComponent></MyComponent>
-      </div>
-    )
-  }
+const App = () => {
 
 
-  // const App = () => {
-  //   const count = useSelector(state => state.counter.count);
-  //   const dispatch = useDispatch();
+  return (
 
-  //   return (
-
-  //     // <div className="App">
-  //     //   <header className="App-header">
-  //     //     <img src={logo} className="App-logo" alt="logo" />
-  //     //     <p>
-  //     //       Quan nè
-  //     //     </p>
-  //     //     <div>Count = {count}</div>
-  //     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-  //     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-  //     //   </header>
-  //     // </div>
-  //   );
+    <div className="app-container">
+      Hello Quan
+    </div>
+  );
 }
 
 export default App;
--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,45 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfo.scss';
 import logo from './../logo.svg';
-
-// class DisplayInfo extends React.Component {
-
-//     render() {
-//         console.log(">>> call me ")
-//         //destructoring array/object
-//         const { listUser } = this.props; //object
-//         // console.log(listUser)
-//         // const listUser = this.props.listUser;
-//         //props => viet tat properties
-//         // console.table(listUser)
-
-//         //template + logic js
-//         return (
-//             <div className="display-info-container">
-
-//                 {true &&
-//                     <>
-//                         {listUser.map((user, index) => {
-//                             return (
-//                                 <>
-//                                     <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                                         <div>My name's {user.name}</div>
-//                                         <div>My age's {user.age}</div>
-
-//                                     </div>
-//                                     <div>
-//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-//                                         <hr />
-//                                     </div>
-//                                 </>
-//                             )
-//                         })}
-//                     </>
-//                 }
-//             </div>
-//         )
-//     }
-// }
 
 const DisplayInfo = (props) => {
     const { listUser } = props; //object
@@ -48,6 +9,18 @@ const DisplayInfo = (props) => {
     const handleShowHideListUser = () => {
         setShowHideListUser(!isShowHideListUser);
     }
+
+    console.log(">>> call me render")
+
+    useEffect(
+        () => {
+            if (listUser.length === 0) {
+                alert('Dont kill me')
+            }
+            console.log(">>> call me effect")
+        }, [listUser]
+    );
+
     return (
         <div className="display-info-container">
             <div>
